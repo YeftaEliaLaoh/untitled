0522012069ce8448b9ee461982a64651f7aa5380
Yefta Elia <yafta.elia@1rstwap.com>
2020-07-07 20:25:10 +0700

    2

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "net/http"
+import 
+(
+	"net/http"
+	"sync"
+)
+
+type Quest1 struct{
+	tes 	string
+}
+
+type questHandlers struct{
+	sync.Mutex
+	store map[string]Quest1
+}
+
+func (q *questHandlers) getQuestion(w http.ResponseWriter, r *http.Request){ 
+	
+	w.Write([]byte("<html><h1>tes1</h1></html>"))
+}
+
+func newQuestion1() *questHandlers{
+	return &questHandlers{
+		store: map[string]Quest1{
+		},
+	}
+}
 
 func main() {
+	question1 := newQuestion1()
+	http.HandleFunc("/question1",question1.getQuestion)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil{
 		panic(err)
