c6f6d36d577b103eb11eeba0cf19b5b28a7f71d0
Yefta Elia <yafta.elia@1rstwap.com>
2020-07-07 20:35:20 +0700

    5

--- main.go
+++ main.go
@@ -48,11 +48,34 @@ func newQuestion2() *questHandlers2{
 	}
 }
 
+type Quest3 struct{
+	tes	string	`json:"name"`
+}
+
+type questHandlers3 struct{
+	sync.Mutex
+	store map[string]Quest3
+}
+
+func (q *questHandlers3) getQuestion(w http.ResponseWriter, r *http.Request){ 
+	w.Header().Add("content-type","application/json")
+	w.WriteHeader(http.StatusOK)
+}
+
+func newQuestion3() *questHandlers3{
+	return &questHandlers3{
+		store: map[string]Quest3{
+		},
+	}
+}
+
 func main() {
 	question1 := newQuestion1()
 	question2 := newQuestion2()
+	question3 := newQuestion3()
 	http.HandleFunc("/question1",question1.getQuestion)
 	http.HandleFunc("/question2",question2.getQuestion)
+	http.HandleFunc("/question3",question3.getQuestion)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil{
 		panic(err)
